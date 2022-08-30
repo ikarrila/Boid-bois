05a3afd9a88068c58a5a137cbd8dcb88dc966fd8
ikarrila <47640841+ikarrila@users.noreply.github.com>
2022-08-30 22:44:11 +0300

    Drawing boids

--- boid.py
+++ boid.py
@@ -1,5 +1,11 @@
+from p5 import Vector, stroke, circle
 
 #creating a class for boid with a position and velocity as vector
 class Boid():
     def __init__(self, x, y, width, height):
         self.position = Vector(x, y)
+
+
+    def show(self):
+        stroke(255)
+        circle((self.position.x, self.position.y), radius=10)
--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from p5 import size
 #opening a window
 def setup():
     size(width, height)
+
+def draw():
+    for boid in flock:
+        boid.show()
